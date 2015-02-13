fd6ef8d7c7dabf19d762e558b84725049c1a63c8
Tiffany Chiou <tiffanichiou@gmail.com>
2015-02-12 23:12:56 -0800

    Added functionality to delete the table and to display all the videos and delete them one by one with a delete button

--- src/videos.php
+++ src/videos.php
@@ -33,38 +33,143 @@ this connects to the database, connection code is based on lines 1-6 in Example 
         </p>
       </form>
 <?php
-$name=$_POST['name'];
-$category=$_POST['category'];
-$length=$_POST['length'];
+$flag1 = false;
 
-if (empty($_POST['name']) || empty($_POST['category']) || empty($_POST['length'])) {
-  echo "Please fill in all required fields.";
-  $_POST = array();
-  die();
+if (isset($_POST['name'])|| isset($_POST['category']) || isset($_POST['length'])) {
+  if (empty($_POST['name']) || empty($_POST['category']) || empty($_POST['length'])) {
+    echo "Please fill in all required fields";
+    $_POST = array(); // empty the POST array
+  }
+  else {
+    if(!ctype_digit($_POST['length'])) {
+      echo "Length must be a numerical value";
+      $_POST = array();
+    }
+    else {
+      if ($_POST['length'] < 0) {
+        echo "Length must be a value greater than 0.";
+        $_POST = array();
+      }
+      else {
+        $flag1 = true;
+      }
+    }    
+
+  }
+
 }
+
+if ($flag1 === true) {
+  $name=$_POST['name'];
+  $category=$_POST['category'];
+  $length=$_POST['length'];
+
+  if (!($add = $connect->prepare("INSERT INTO inventory(name, category, length) VALUES (?, ?, ?)"))) {
+    echo "Insert Preparation Error";
+  }
  
-if (ctype_punct($name) || ctype_punct($category)) {
-  echo "You have entered invalid characters. Please try again.";
-  die();
-}
-// This makes sure that the name and category values are not completely made of punctuation symbols, and that category is completely made of letters.
+  if (!($add->bind_param("ssi", $name, $category, $length))) {
+    echo "Binding Parameters Error";
+  }
 
-if (!($add = $connect->prepare("INSERT INTO inventory(name, category, length) VALUES (?, ?, ?)"))) {
+  if (!($add->execute())) {
+    echo "Execute Error";
+  }
+// the three if statements above add movies to the database 
+  $add->close();
+  $flag1 = false;
+}
+
+?>
+    </p>
+    <p>
+      <h3>Delete All Videos</h3>
+      <form method="POST" action="videos.php">
+        <input type="submit" name="deleteAll" value="Delete All">
+      </form>
+    </p>
+<?php
+if (isset($_POST['deleteAll']) && $_POST['deleteAll']== "Delete All" ) {
+ 
+  if (!($del = $connect->prepare("DELETE FROM inventory"))) {
+    echo "Insert Preparation Error";
+  }
+ 
+  if (!($del->execute())) {
+    echo "Execute Error";
+  }
+// this deletes ALL the videos from the database 
+
+  $del->close();
+  $_POST = array(); // clear the array just in case
+}
+
+if (!($count = $connect->prepare("SELECT COUNT(*) FROM inventory"))) {
   echo "Insert Preparation Error";
 }
  
-if (!($add->bind_param("ssi", $name, $category, $length))) {
-  echo "Binding Parameters Error";
-}
-
-if (!($add->execute())) {
+if (!($count->execute())) {
   echo "Execute Error";
 }
-// the three if statements above add movies to the database 
+
+if (!($count->bind_result($number))) {
+  echo "Error retrieving results";
+}
+
+$count->fetch();
+// this code above will analyze how many entries are in the table
+
+$count->close();
+
+if ($number > 0) {
+
+  echo "<table border=1>";
+
+  if (!($ret = $connect->prepare("SELECT id, name, category, length, rented FROM inventory"))) {
+    echo "Insert Preparation Error";
+  }
+ 
+  if (!($ret->execute())) {
+    echo "Execute Error";
+  } 
+
+  if (!($ret->bind_result($vId, $vidN, $vidC, $vidL, $vidR))) {
+    echo "Error retrieving results";
+  }
+
+  while ($ret->fetch()) {
+
+    //if ($vidR == 1)
+
+    echo "<tr><td>$vidN<td>$vidC<td>$vidL<td>$vidR<td><form method=\"POST\" action=\"videos.php\"><input type=\"hidden\" name=\"id\" value=\"$vId\"><input type=\"submit\" name=\"deleteOne\" value=\"Delete\"></form></td></tr>";
+  }
+  echo "</table>";
+  
+  $ret->close();
+// the code here should fetch the rows - fetching all rows for now to make sure code works
+
+  if (isset($_POST['id'])) {
+    $vidId =  $_POST['id'];
+    if (!($delO = $connect->prepare("DELETE FROM inventory WHERE id=(?)"))) {
+      echo "Insert Preparation Error";
+    }
+ 
+    if (!($delO->bind_param("i", $vidId))) {
+      echo "Binding Parameters Error";
+    }
+
+    if (!($delO->execute())) {
+      echo "Execute Error";
+    } 
+
+    $delO->close();
+
+    $_POST = array();
+  }
+  // this deletes the video based on its id
+
+}
 
 ?>
-
-    </p>
-
   </body>
 </html>
